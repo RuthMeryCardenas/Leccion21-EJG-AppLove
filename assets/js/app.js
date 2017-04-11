0aec2cea3fe6fe158d8d8b8eae491e4ec2120cf7
RuthMery <ruth.cardenas.perez@gmail.com>
2017-04-11 14:41:43 -0500

    Implementación Modal

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+//VARIABLES GLOBALES
+var productsFigure = document.getElementsByClassName("img-product");
+//EVENTOS
+productsFigure[0].addEventListener("click", function (){
+	console.log("click en figura 1");
+});
+productsFigure[1].addEventListener("click", function (){
+	console.log("click en figura 2");
+});
