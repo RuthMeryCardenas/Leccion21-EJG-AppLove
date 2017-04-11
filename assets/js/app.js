6cd438037b0c3b9375b971d38a98456e16250e56
RuthMery <ruth.cardenas.perez@gmail.com>
2017-04-11 15:55:40 -0500

    Probando modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,17 @@
 //VARIABLES GLOBALES
 var productsFigure = document.getElementsByClassName("img-product");
+var modalDiv = document.getElementById("modal");
+var closeSpan = document.getElementById("close-modal");
 //EVENTOS
+closeSpan.addEventListener("click", function (){
+	console.log("cerrar modal");
+	modalDiv.style.display = "none";
+});
 productsFigure[0].addEventListener("click", function (){
 	console.log("click en figura 1");
+	modalDiv.style.display = "block";
 });
 productsFigure[1].addEventListener("click", function (){
 	console.log("click en figura 2");
+	modalDiv.style.display = "block";
 });
